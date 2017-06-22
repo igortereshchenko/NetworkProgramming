263ea27f92f4f3cf83bf394d88d0079d0095ed0b
igortereshchenko <tereshchenko.igor@gmail.com>
2017-06-22 08:04:33 +0300

    UDP

---
+++ UDPChat/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UDPChat
+{
+    public partial class Form1 : Form
+    {
+
+        bool alive = false; // будет ли работать поток для приема
+        UdpClient client;
+        const int LOCALPORT = 8001; // порт для приема сообщений
+        const int REMOTEPORT = 8001; // порт для отправки сообщений
+        const int TTL = 20;
+        const string HOST = "235.5.5.1"; // хост для групповой рассылки
+        IPAddress groupAddress; // адрес для групповой рассылки
+
+        string userName; // имя пользователя в чате
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            btnEnter.Enabled = true; // кнопка входа
+            btnExit.Enabled = false; // кнопка выхода
+            btnSend.Enabled = false; // кнопка отправки
+            txtMessages.ReadOnly = true; // поле для сообщений
+
+            groupAddress = IPAddress.Parse(HOST);
+        }
+
+        private void btnEnter_Click(object sender, EventArgs e)
+        {
+            userName = txtName.Text;
+            txtName.ReadOnly = true;
+
+            try
+            {
+                client = new UdpClient(LOCALPORT);
+                // присоединяемся к групповой рассылке
+                client.JoinMulticastGroup(groupAddress, TTL);
+
+                // запускаем задачу на прием сообщений
+                Task receiveTask = new Task(ReceiveMessages);
+                receiveTask.Start();
+
+                // отправляем первое сообщение о входе нового пользователя
+                string message = userName + " вошел в чат";
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+
+                btnEnter.Enabled = false;
+                btnExit.Enabled = true;
+                btnSend.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ReceiveMessages()
+        {
+            alive = true;
+            try
+            {
+                while (alive)
+                {
+                    IPEndPoint remoteIp = null;
+                    byte[] data = client.Receive(ref remoteIp);
+                    string message = Encoding.Unicode.GetString(data);
+
+                    // добавляем полученное сообщение в текстовое поле
+                    this.Invoke(new MethodInvoker(() =>
+                    {
+                        string time = DateTime.Now.ToShortTimeString();
+                        txtMessages.Text = time + " " + message + "\r\n" + txtMessages.Text;
+                    }));
+                }
+            }
+            catch (ObjectDisposedException)
+            {
+                if (!alive)
+                    return;
+                throw;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string message = String.Format("{0}: {1}", userName, txtMessage.Text);
+                byte[] data = Encoding.Unicode.GetBytes(message);
+                client.Send(data, data.Length, HOST, REMOTEPORT);
+                txtMessage.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            ExitChat();
+        }
+
+        private void ExitChat()
+        {
+            string message = userName + " покидает чат";
+            byte[] data = Encoding.Unicode.GetBytes(message);
+            client.Send(data, data.Length, HOST, REMOTEPORT);
+            client.DropMulticastGroup(groupAddress);
+
+            alive = false;
+            client.Close();
+
+            btnEnter.Enabled = true;
+            btnExit.Enabled = false;
+            btnSend.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (alive)
+                ExitChat();
+        }
+    }
+}
