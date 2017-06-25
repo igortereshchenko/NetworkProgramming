88e9188a0c3834914a1218367bae41059b9a9be4
igortereshchenko <tereshchenko.igor@gmail.com>
2017-06-25 23:18:42 +0300

    NetStream added

---
+++ BinaryStreamServer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryStreamServer
+{
+   class Program
+    {
+        const int PORT = 5006; // порт для прослушивания подключений
+        static TcpListener listener;
+        static void Main(string[] args)
+        {
+            try
+            {
+                listener = new TcpListener(IPAddress.Parse("127.0.0.1"), PORT);
+                listener.Start();
+                Console.WriteLine("waiting...");
+ 
+                while (true)
+                {
+                    TcpClient client = listener.AcceptTcpClient();
+                    Client clientObject = new Client(client);
+ 
+                    // создаем новый поток для обслуживания нового клиента
+                    Task clientTask = new Task(clientObject.Process);
+                    clientTask.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (listener != null)
+                    listener.Stop();
+            }
+        }
+    }
+}
---
+++ NetworkStreamClient/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkStreamClient
+{
+    class Program
+    {
+        const int PORT = 5006;
+        const string ADDRESS = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            TcpClient client = null;
+            try
+            {
+                Console.Write("Enter message: ");
+                string message = Console.ReadLine();
+                client = new TcpClient(ADDRESS, PORT);
+                NetworkStream stream = client.GetStream();
+
+                // отправляем сообщение
+                StreamWriter writer = new StreamWriter(stream);
+                writer.WriteLine(message);
+                writer.Flush();
+
+                // BinaryReader reader = new BinaryReader(new BufferedStream(stream));
+                StreamReader reader = new StreamReader(stream);
+                message = reader.ReadLine();
+                Console.WriteLine("Answer: " + message);
+
+                reader.Close();
+                writer.Close();
+                stream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (client != null)
+                    client.Close();
+            }
+        }
+    }
+}
